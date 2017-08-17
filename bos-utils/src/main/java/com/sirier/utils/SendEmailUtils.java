97bb5090f402c04257e5219463894b8625153409
Sirierx <xiaosc137520767@gmail.com>
2017-08-17 23:54:38 +0800

    成功添加quartz任务调度功能,执行定时发送邮件任务

--- bos-utils/src/main/java/com/sirier/utils/SendEmailUtils.java
+++ bos-utils/src/main/java/com/sirier/utils/SendEmailUtils.java
@@ -1,10 +1,12 @@
 package com.sirier.utils;
 
+import org.springframework.stereotype.Component;
+
 /**
  * 发送邮件的工具类,已配置到quartz管理执行
  * Created by Sirierx on 2017/8/17.
  */
-
+@Component("sendEmail")
 public class SendEmailUtils {
 
     private static void SendEmail() {
